baddcce01afd3b3c92ef1ba1df0c4d21ba31e0c0
itanujtiwari <tanujtiwari2002@gmail.com>
2023-11-20 12:40:13 +0530

    day 1 - second code(updated)

--- Swap.java
+++ Swap.java
@@ -2,12 +2,11 @@ public class Swap {
     public static void main(String[] args) {
         int n1 = 10;
         int n2 = 20;
-        int vada;
         System.out.println("Before swap " + "n1 : " +n1  + " n2 : " +n2);
-
-        vada = n2;
-        n2 = n1;
-        n1 = vada;
+        
+        n1 = n1 + n2; //10 + 20
+        n2 = n1 - n2; //30 - 20
+        n1 = n1 - n2; //30 - 10
         
         System.out.println("After swap " + "n1 : " +n1  + " n2 : " +n2);
 
