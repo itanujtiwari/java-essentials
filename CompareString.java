0a1cc73c2af4fbec7757fd3f77f6c0bc0b1c424a
itanujtiwari <tanujtiwari2002@gmail.com>
2023-11-27 01:44:44 +0530

    compare

---
+++ CompareString.java
@@ -0,0 +1,11 @@
+public class CompareString {
+    
+    public static void main(String[] args) {
+        String s1 = "YOYO";
+        String s2 = "YOLO";
+        String s3 = "YOYO";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+    }
+}
